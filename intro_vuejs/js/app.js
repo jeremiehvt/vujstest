0dd09e370c45e71b9bfc9f5cd5ab9a3fb0e44f87
jeremiehvt <jeremiehvt@gmail.com>
2019-09-19 14:44:29 +0200

    new organazition of folders and files

---
+++ intro_vuejs/js/app.js
@@ -0,0 +1,45 @@
+let vm = new Vue({
+    el: '#app',
+    data: {
+        message: 'world',
+        link: 'https://www.google.fr/',
+        success: 'error',
+        persons: [
+            'me',
+            'you',
+            'us'
+        ],
+        flash: false,
+        flashMessage: 'news infos',
+        successTwo: false,
+        messageTwo: '',
+        firstName: 'me',
+        lastName: 'you'
+    },
+
+    methods: {
+        close: function () {
+            this.success = false
+        },
+        addPerson: function() {
+            this.persons.push('another')
+        } 
+    },
+
+    computed: {
+        // cls: function () {
+        //     return this.successTwo === true ? 'success' : 'error'
+        // },
+        fullname: {
+            get: function () {
+                return this.firstName +'  '+ this.lastName
+            },
+            set: function (value) {
+                let parts = value.split(' ')
+                this.firstName = parts[0]
+                this.lastName = parts[parts.length - 1]
+            }
+        }
+    },
+});
+
