f29d9e79f4719b06bc27eee7cbcfd888663207b2
jeremiehvt <jeremiehvt@gmail.com>
2019-09-19 17:40:58 +0200

    test vue js filters

--- filters/js/app.js
+++ filters/js/app.js
@@ -1,28 +1,18 @@
-salut = function (el, binding) {
-        console.log(el);
-        console.log(binding);
-
-        el.value = binding.value;
-    }
+let capitalize =  function (value, preffix, suffix) {
+    console.log(preffix, suffix);
+        return preffix+value.toUpperCase(value)+suffix;
+};
 
 let vm = new Vue({
     el: '#app',
-    directives: {
-        salut: salut
-    },
+    
     data: {
         message: 'hello',
-        message2: 'bye',
-        message3: 'good',
+       
     },
 
-    methods: {
-        demo: function (e) {
-            console.log(this.message)
-        },
-        demo2: function (e) {
-            console.log(this.message2)
-        }
+    filters: {
+            capitalize
     }
  
 });
