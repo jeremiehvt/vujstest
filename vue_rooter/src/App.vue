eda3dfe66dd76f33c52caf3c551e340d0dfae854
jeremiehvt <jeremiehvt@gmail.com>
2019-10-01 16:59:27 +0200

    change link in vue router

--- vue_rooter/src/App.vue
+++ vue_rooter/src/App.vue
@@ -5,15 +5,15 @@
         <h1>sidebar</h1>
         <router-view name="sidebar"></router-view>
       </div>-->
+      <div class="menu">
+        <router-link :to="{name: 'root'}">accueil</router-link>
+        <br />
+        <br />
+        <router-link :to="{name: 'post_un', params: {id:1}}">article1</router-link>
+      </div>
       <div class="main">
         <h1>MAin</h1>
         <router-view></router-view>
-      </div>
-      <div class="menu">
-        <router-link to="{name: 'root'}">accueil</router-link>
-        <br />
-        <br />
-        <router-link to="{name: 'post', params: {id:'1'}}">article1</router-link>
       </div>
     </v-content>
     <!-- <pre>{{JSON.stringify($route, null, 2)}}</pre> -->
